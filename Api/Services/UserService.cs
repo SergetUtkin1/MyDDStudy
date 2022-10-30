6e3ef6212ce40cc217e302ce55d69667eee704a7
Sergey_Utkin <sergey.uty.uty@gmail.com>
2022-10-31 00:52:57 +0300

    Add project files.

---
+++ Api/Services/UserService.cs
@@ -0,0 +1,32 @@
+﻿using Api.Models;
+using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using DAL;
+using Microsoft.EntityFrameworkCore;
+
+namespace Api.Services
+{
+    public class UserService
+    {
+        private readonly DAL.DataContext _context;
+        private readonly IMapper _mapper;
+
+        public UserService(DataContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        public async Task CreateUser(CreateUserModel model)
+        {
+            var DBuser = _mapper.Map<DAL.Entities.User>(model);
+            await _context.AddAsync(DBuser);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<List<UserModel>> GetUsers()
+        {
+            return await _context.Users.AsNoTracking().ProjectTo<UserModel>(_mapper.ConfigurationProvider).ToListAsync();
+        }
+    }
+}
